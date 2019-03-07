1bb04375c338a13395bf39b8861a3938e39ca4b5
NYStimulus@yandex.ru <FaHrEnHeIt21>
2019-03-07 17:56:14 +0300

    ДЗ 3.

--- Homework_3.java
+++ Homework_3.java
@@ -4,31 +4,43 @@ import java.util.Scanner;
 
 public class Homework_3 {
 
-    private  static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int hiddenNumber = 0;
+        do{
+            playGuessTheNumber();
+        }while(repeatGame());
+        Scanner close;
+    }
+    private static void playGuessTheNumber(){
         int status = 3;
-        hiddenNumber = randomNumber(hiddenNumber);
         System.out.print("Угадайте число от 0 до 9. ");
+        int hiddenNumber = (int)Math.ceil(Math.random() * 9);
         checkStatus(status);
-        for(status = 3; status > 0;){
+        while (status > 0){
             int enteredNumber = scanner.nextInt();
-            if(hiddenNumber == enteredNumber){
+            if (hiddenNumber == enteredNumber) {
                 System.out.println("Вы отгадали!");
                 break;
-            } else{
-                System.out.println("Неверно!");
+            } else {
                 --status;
+                MoreOrLess(enteredNumber, hiddenNumber);
                 checkStatus(status);
             }
         }
     }
-    private static int randomNumber(int hiddenRandom){
-        return hiddenRandom = (int)Math.ceil(Math.random() * 9);
-    }
     private static void checkStatus(int status){
-        System.out.println("У вас " + status + " попытки.");
+        System.out.println("У вас " + status + " попыток.");
     }
-
+    private static boolean repeatGame(){
+        System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+        boolean repeat = false;
+        int answer = scanner.nextInt();
+        repeat = (answer == 1) ? true : false;
+        return repeat;
+    }
+    private static void MoreOrLess(int enteredNumber, int hiddenNumber){
+        String answer = (enteredNumber > hiddenNumber) ? "Неверно! Загаданное число меньше!" : "Неверно! Загаданное число больше!";
+        System.out.println(answer);
+    }
 }
